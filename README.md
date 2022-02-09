ba4d9b8f6fb053742d254cd430341040ac54f4e5
yangjt <yangjt@yoozoo.com>
2022-02-09 14:33:29 +0800

    init/add: add cas case

---
+++ README.md
@@ -0,0 +1,8 @@
+# trial-mission
+trial more frameworks of auth
+
+includes: 
+1. trial-cas: case of server & client
+2. trial-oauth2
+3. trial-shiro
+3. trial-spring-security
---
+++ trial-cas/trial-client-crm/src/main/java/lewy/CasClientCrmApplication.java
@@ -0,0 +1,15 @@
+package lewy;
+
+import net.unicon.cas.client.configuration.EnableCasClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableCasClient
+public class CasClientCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasClientCrmApplication.class, args);
+    }
+
+}
